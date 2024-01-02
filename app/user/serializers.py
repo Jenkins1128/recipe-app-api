938e1dcb2ccdd8caf3ee586423bea1739633a411
Isaiah Jenkins <35716724+Jenkins1128@users.noreply.github.com>
2024-01-02 11:50:11 -0800

    Fixed RecipeViewSet serializer and other small fixes

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,14 +18,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-    def create(self, validate_data):
+    def create(self, validated_data):
         """Create and return a user with encrypted password."""
-        return get_user_model().objects.create_user(**validate_data)
+        return get_user_model().objects.create_user(**validated_data)
 
-    def update(self, instance, validate_data):
+    def update(self, instance, validated_data):
         """Update and return user."""
-        password = validate_data.pop('password', None)
-        user = super().update(instance, validate_data)
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
